83813b049872f6bef60073a95cfa402dc8e589d4
Валентина <abc99-33@yandex.ru>
2022-08-07 20:03:08 +0300

    Lessons1 завершён

--- Lesson0/Program.cs
+++ Lesson0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат.
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} = {square}");
